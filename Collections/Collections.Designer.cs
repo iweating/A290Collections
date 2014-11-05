455fb3c6f288f8a3567ae1818b428c6da72e2af1
iweating <iweating@indiana.edu>
2014-11-05 14:53:43 -0500

    Initial Collections Commit

---
+++ Collections/Collections.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Collections
+{
+    partial class frmCollections
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowNames = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnShowNames
+            // 
+            this.btnShowNames.Location = new System.Drawing.Point(88, 112);
+            this.btnShowNames.Name = "btnShowNames";
+            this.btnShowNames.Size = new System.Drawing.Size(120, 23);
+            this.btnShowNames.TabIndex = 0;
+            this.btnShowNames.Text = "Show Control Names";
+            this.btnShowNames.UseVisualStyleBackColor = true;
+            this.btnShowNames.Click += new System.EventHandler(this.btnShowNames_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(12, 32);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Button 1";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(107, 32);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "Button 2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(197, 32);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "Button 3";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(59, 61);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 23);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "Button 4";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(154, 61);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 23);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "Button 5";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(12, 155);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(260, 79);
+            this.txt1.TabIndex = 6;
+            this.txt1.Text = "This is the initialized text.";
+            // 
+            // frmCollections
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btnShowNames);
+            this.Name = "frmCollections";
+            this.Text = "Collections Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowNames;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.TextBox txt1;
+    }
+}
+
---
+++ Collections/Collections.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Collections
+{
+    public partial class frmCollections : Form
+    {
+        public frmCollections()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowNames_Click(object sender, EventArgs e)
+        {
+            for(int intIndex = 0;intIndex<this.Controls.Count;intIndex++)
+            {
+                MessageBox.Show("Control #" + intIndex.ToString() + " has the name " + this.Controls[intIndex].Name);
+            }
+        }
+    }
+}
